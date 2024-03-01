c1898544c68075a63f5169a2ee069f01a049739e
s21sd <srivastavasunny359@gmail.com>
2024-03-01 21:14:26 +0530

    dsds

--- backend/src/middlewares/checkAuthMiddleware.ts
+++ backend/src/middlewares/checkAuthMiddleware.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express";
+import dotenv, { config } from 'dotenv'
+config();
 import jwt, { Secret } from "jsonwebtoken";
 
 interface AuthenticatedRequest extends Request {
@@ -7,7 +9,7 @@ interface AuthenticatedRequest extends Request {
 
 const checkAuth = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
     console.log("CheckAuth called")
-    const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWQ0NTY3YTZhZjg2MThiODY0NTFjZGEiLCJlbWFpbCI6InN1bm55c3JpdmFzdGF2YTI1OEBnbWFpbC5jb20iLCJpYXQiOjE3MDg2MTE3MjYsImV4cCI6MTcwODYxMjMyNn0.rPaYumAkTNP8d9gZZ-fV857Y_t6choEz2YnoSCijzAQ'
+    const authToken = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJkZXYiOiJkdmJfMmNGcnJWemhDUW0ycXZjY0N0ZmlrRnRxeXJEIiwiaWQiOiJjbGllbnRfMmNZVzhRTUV6SEpsVFJtS0dkWGFTZDBRWlpmIiwicm90YXRpbmdfdG9rZW4iOiIydTFhMjg4amFxdXdmdzB0Y3lseGN0djEyZHUxMXliYm8wazIwcjZnIn0.uUnm3QKE57y-Rswo8F37hXZAO0tzgvSC1QkPfrXeIIO1rqyDoRUrAe3iKT6R4EpvOZKwVY7mim_XuVUNvNv9wNtKPsQg_pv_ZkQYQG3aXlYNl3E9CbkcNqlAMr-3nii0bgayovi7mIalSnzod1t3v4R8YFRLuREEm_x-4-hNUv7O1Z9iYx5RF1rgi5Ei9FwNFaN8Om5CLtlpY8yxVzkGU7IO2U3tB1IiYlAZSnVInjUGADHw9LaSywbDP3z3vdtCMcyF68MLLAIbm7xVOJE6dR2GQrKMnuftz8qyo_hqHn6Q_BqLLZyCNmSc8BuQOeZesx5XkmtW6HZFvXrcxTvjtw'
     // const authToken = req.cookies.token;
     // console.log("Token", authToken);
     if (!authToken) {
