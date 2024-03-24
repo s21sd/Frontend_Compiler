cac4aad1b0c1c59ab9ec53b5ef142c97ee37c8b8
s21sd <srivastavasunny359@gmail.com>
2024-03-24 13:05:54 +0530

    ff

--- frontend_compiler/src/redux/slices/comilerSlice.ts
+++ frontend_compiler/src/redux/slices/comilerSlice.ts
@@ -24,7 +24,7 @@ const initialState: initialStatetype = {
 </html>`,
         css: `body {
             font-family: Arial, sans-serif;
-            background-color: #f0f0f0;
+            background-color: #6a5acd;
             margin: 0;
             padding: 0;
         }
