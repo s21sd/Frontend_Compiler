049c56cdc90402ef567b89da82fb3c3e39eca5bb
Sunny Srivastava <98369603+s21sd@users.noreply.github.com>
2024-04-10 00:51:33 +0530

    Update index.js
    h

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -15,7 +15,7 @@ app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
     credentials: true,
-    origin: 'https://frontend-compiler-m7r6i578c-s21sds-projects.vercel.app'
+    origin: 'https://frontend-compiler.vercel.app'
 }));
 (0, dotenv_1.config)();
 (0, dbConnect_1.connectToDB)();
