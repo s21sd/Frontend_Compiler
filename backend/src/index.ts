3776356c72251a5dae48ec9dea505bf263a98adc
Sunny Srivastava <98369603+s21sd@users.noreply.github.com>
2024-04-10 00:44:56 +0530

    Update index.ts
    nns

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,7 +17,7 @@ app.use(cookieParser());
 // }));
 app.use(cors({
     credentials: true,
-    origin: 'https://web-compiler-rk73yvl65-s21sds-projects.vercel.app'
+    origin: 'https://frontend-compiler-m7r6i578c-s21sds-projects.vercel.app'
 }));
 
 
