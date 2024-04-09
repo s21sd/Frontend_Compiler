5b3deed69fb45ee503a63f3c1293de9828222991
Sunny Srivastava <98369603+s21sd@users.noreply.github.com>
2024-04-09 13:08:33 +0530

    Update index.js
    g

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -15,7 +15,7 @@ app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
     credentials: true,
-    origin: 'http://localhost:5173'
+    origin: '*'
 }));
 (0, dotenv_1.config)();
 (0, dbConnect_1.connectToDB)();
